0236aab54582110e7dae845e9a32a5068487b87f
Ricardo Romaní Trillo <raimangsxr@gmail.com>
2026-02-22 20:24:33 +0100

    Fixed Material errors

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,9 @@
+import 'zone.js';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, RouterOutlet } from '@angular/router';
 
 import { authInterceptor } from './app/core/auth.interceptor';
@@ -15,5 +18,13 @@ import { routes } from './app/routes';
 class AppComponent {}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' }
+    }
+  ]
 }).catch((err) => console.error(err));
